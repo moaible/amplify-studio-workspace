114d956689cb76f19c0dc4c1fcf909f8a6ce62aa
moaible <moai.motodera@gmail.com>
2021-12-06 10:42:09 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Ampligram } from './ui-components';
+
+function App() {
+  return (
+    <Ampligram imageURL="https://cdn-ssl-devio-img.classmethod.jp/wp-content/uploads/2021/12/invite-user.png"></Ampligram>
+  );
+}
+
+export default App;
